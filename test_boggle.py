3028d00dd6e4ceac8586eb8c67758a243808514f
Marc Beckerton <marcbeckerton@hotmail.com>
2018-10-10 18:38:16 +0000

    commit

--- test_boggle.py
+++ test_boggle.py
@@ -1,6 +1,6 @@
 import unittest
-
 import boggle
+from string import ascii_uppercase
 
 class TestBoggle(unittest.TestCase):
     """ 
@@ -31,3 +31,64 @@ class TestBoggle(unittest.TestCase):
         self.assertIn((1, 0), grid)
         self.assertIn((1, 1), grid)
         self.assertNotIn((2, 2), grid)
+        
+    def test_grid_is_filled_with_letters(self):
+        """
+        ensure that each of the coordinates in the grid contain letters
+        """
+        grid = boggle.make_grid(2, 3)
+        for letter in grid.values():
+            self.assertIn(letter, ascii_uppercase)
+            
+    def test_neighbours_of_a_position(self):
+        """
+        ensure that a position has 8 neighbours
+        """
+        coords = (1, 2)
+        neighbours = boggle.neighbours_of_position(coords)
+        self.assertIn((0, 1), neighbours)
+        self.assertIn((0, 2), neighbours)
+        self.assertIn((0, 3), neighbours)
+        self.assertIn((1, 1), neighbours)
+        self.assertIn((1, 3), neighbours)
+        self.assertIn((2, 1), neighbours)
+        self.assertIn((2, 2), neighbours)
+        self.assertIn((2, 3), neighbours)
+        
+    def test_all_grid_neighbours(self):
+        """
+        ensure that all of the grid positions have neighbours
+        """
+        grid = boggle.make_grid(2, 2)
+        neighbours = boggle.all_grid_neighbours(grid)
+        self.assertEqual(len(neighbours), len(grid))
+        for pos in grid:
+            others = list(grid) #creates new list from the dictionaries keys
+            others.remove(pos)
+            self.assertListEqual(sorted(neighbours[pos]), sorted(others))
+            
+    def test_converting_a_path_to_a_word(self):
+        """
+        ensure that paths can be converted to words
+        """
+        grid = boggle.make_grid(2, 2)
+        oneLetterWord = boggle.path_to_word(grid, [(0, 0)])
+        twoLetterWord = boggle.path_to_word(grid, [(0, 0), (1, 1)])
+        self.assertEqual(oneLetterWord, grid[(0, 0)])
+        self.assertEqual(twoLetterWord, grid[(0, 0)] + grid[(1, 1)])
+        
+    def test_search_grid_for_words(self):
+        """
+        ensure that certain patterns can be found in a path_to_word
+        """
+        grid = {(0, 0): 'A', (0, 1): 'B', (1, 0): 'C', (1, 1): 'D'}
+        twoLetterWord = 'AB'
+        threeLetterWord = 'ABC'
+        notThereWord = 'EEE'
+        dictionary = [twoLetterWord, threeLetterWord, notThereWord]
+        
+        foundWords = boggle.search(grid, dictionary)
+        
+        self.assertTrue(twoLetterWord in foundWords)
+        self.assertTrue(threeLetterWord in foundWords)
+        self.assertTrue(notThereWord not in foundWords)
